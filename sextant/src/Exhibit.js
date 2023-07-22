1c3d9186a65a0523daea9f5bab5647805a84fa36
AsadShahid204 <asad.shahid@mail.sadiq.ai>
2023-07-21 18:26:53 -0700

    Added changes to the Banenr and Exhibit components

--- sextant/src/Exhibit.js
+++ sextant/src/Exhibit.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Exhibit.css";
 
-const Exhibit = ({ title, children }) => {
-  return (
-    <div className="exhibit">
-      <h2>{title}</h2>
-      {children}
-    </div>
-  );
-};
+class Exhibit extends Component {
+  render() {
+    return (
+      <div className="Exhibit">
+        <h2 className="ExhibitHeading">{this.props.name}</h2>
+        <div className="ExhibitContent">{this.props.children}</div>
+      </div>
+    );
+  }
+}
 
 export default Exhibit;
